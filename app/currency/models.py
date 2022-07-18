992a09e751ae7ca2965780d361bee8c4f5f0d511
kur1lo <kuriloroma@gmail.com>
2022-07-18 12:30:10 +0300

    1

---
+++ app/currency/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class ContactUs(models.Model):
+    # id = models.CharField(max_length=200)
+    email_to = models.EmailField(max_length=254)
+    email_from = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=200)
+    massage = models.CharField(max_length=500)
+
+
