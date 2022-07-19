9add4e5419a930391e1dd43ee8c7bd98fe26bc90
kur1lo <kuriloroma@gmail.com>
2022-07-19 14:14:51 +0300

    1

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from currency.models import ContactUs
 
 
 def first_func(request):
-    return HttpResponse('Hello World')
+    return HttpResponse("Hello world")
+
+
+def contact_base(request):
+    base_list = []
+    for data in ContactUs.objects.all():
+        redy_string = f'<br>{data.id}, {data.email_to}, {data.email_from}, {data.subject}, {data.massage} '
+        base_list.append(redy_string)
+    return HttpResponse(str(base_list))
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import first_func
+from currency.views import first_func, contact_base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('first_func/', first_func)
+    path('first_func/', first_func),
+
+    path('contact_base/', contact_base)
 ]
