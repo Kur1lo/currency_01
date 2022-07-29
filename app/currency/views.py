1063734cf03aca713d691584fa81b871a0266679
kur1lo <kuriloroma@gmail.com>
2022-07-29 23:50:12 +0300

    1

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
 
 from currency.models import ContactUs, Rate, Source
+from currency.forms import RateForm
 
 
 def first_func(request):
@@ -27,8 +28,48 @@ def rate_list(request):
 
 
 def rate_create(request):
-    context = {}
+    if request.method == "POST":
+
+        form = RateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/rate/")
+    elif request.method == "GET":
+        form = RateForm(request.GET)
+    context = {"form": form}
     return render(request, 'rate_create.html', context=context)
+
+
+def rate_update(request, rate_id):
+
+    rate_instance = get_object_or_404(Rate, id=rate_id)
+
+    if request.method == "POST":
+        form = RateForm(request.POST, instance=rate_instance)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/rate/")
+    elif request.method == "GET":
+        form = RateForm(request.GET, instance=rate_instance)
+
+    context = {"form": form}
+    return render(request, 'rate_update.html', context=context)
+
+
+def rate_details(request, rate_id):
+    rate_instance = get_object_or_404(Rate, id=rate_id)
+    context = {'instance': rate_instance}
+    return render(request, 'rate_details.html', context=context)
+
+
+def rate_delete(request, rate_id):
+    rate_instance = get_object_or_404(Rate, id=rate_id)
+
+    if request.method == 'POST':
+        rate_instance.delete()
+        return HttpResponseRedirect('/rate/')
+    context = {'instance': rate_instance}
+    return render(request, 'rate_delete.html', context=context)
 
 
 def source_data(request):
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import first_func, contact_base, rate_list, index, source_data, rate_create
+from currency.views import first_func, contact_base, rate_list, index, source_data, rate_create, rate_update, rate_details, rate_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +13,13 @@ urlpatterns = [
 
     path('rate/rate_create/', rate_create),
 
+    path('rate/update/<int:rate_id>/', rate_update),
+
+    path('rate/details/<int:rate_id>/', rate_details),
+
+    path('rate/delete/<int:rate_id>/', rate_delete),
+
     path('source/', source_data),
 
-    path('', index)
+    path('', index),
 ]
