adacdbc222469312abb37b5e42bb246ddc76b4fd
kur1lo <kuriloroma@gmail.com>
2022-09-12 13:47:53 +0300

    Dz 16

--- app/currency/views.py
+++ app/currency/views.py
@@ -3,7 +3,9 @@ from django import forms
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django_filters.views import FilterView
 
+from currency.filters import RateFilter
 from currency.models import ContactUs, Rate, Source, ResponseLog
 from currency.forms import RateForm, SourceForm, ContactUsForm
 
@@ -38,9 +40,33 @@ class ContactUsCreateView(generic.CreateView):
         return response
 
 
-class RateListView(generic.ListView):
+class RateListView(FilterView):
     queryset = Rate.objects.all().select_related('source')
     template_name = 'rate_list.html'
+    paginate_by = 10
+    filterset_class = RateFilter
+    page_size_options = ['4', '8', '12', '24', '36']
+
+    def get_context_data(self, *args, **kwargs):
+        context: dict = super().get_context_data(*args, **kwargs)
+        filters_params = self.request.GET.copy()
+
+        if self.page_kwarg in filters_params:
+            del filters_params[self.page_kwarg]
+
+        context['filters_params'] = filters_params.urlencode()
+        context['page_size'] = self.get_paginate_by()
+        context['page_size_options'] = self.page_size_options
+
+        return context
+
+    def get_paginate_by(self, queryset=None):
+        if self.request.GET.get('page_size') in self.page_size_options:
+            paginate_by = self.request.GET['page_size']
+        else:
+            paginate_by = self.paginate_by
+
+        return paginate_by
 
 
 class RateCreateView(generic.CreateView):
