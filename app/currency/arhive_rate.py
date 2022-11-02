c72534b84c785fa5c82eb720b047af538f107ae0
kur1lo <kuriloroma@gmail.com>
2022-11-02 15:39:43 +0200

    dz 21

--- app/currency/arhive_rate.py
+++ app/currency/arhive_rate.py
@@ -1,69 +1,80 @@
-# from currency.models import Rate, Source
-# from currency import consts
-#
-# import requests
-#
-# from currency.utils import to_decimal
-#
-#
-# def parse_url(day, month, year):
-#     url = f"https://api.privatbank.ua/p24api/exchange_rates?json&date={day}.{month}.{year}"
-#     response = requests.get(url)
-#     print(response.json())
-#     response_data = response.json()
-#     write_to_db(response_data, url)
-#     return date_for_parse(response)
-#
-#
-# def date_for_parse(response):
-#     date = response.json()['date']
-#     date_my = []
-#
-#     for x in date.split('.'):
-#         date_my.append(x)
-#
-#     day = int(date_my[0])
-#     month = int(date_my[1])
-#     year = int(date_my[2])
-#
-#     return parse_url(day - 1, month, year)
-#
-#
-# def write_to_db(response_data, url):
-#
-#     source = Source.objects.get_or_create(
-#         name=consts.CODE_NAME_PRIVATBANK,
-#         defaults={'source_url': url, 'name': 'PrivatBank'},)[0]
-#
-#     for rate_data in response_data['exchangeRate'][1:]:
-#         currency_type = rate_data['currency']
-#         base_currency_type = rate_data['baseCurrency']
-#
-#         buy = to_decimal(rate_data['purchaseRateNB'])
-#         sale = to_decimal(rate_data['saleRateNB'])
-#         created = response_data['date']
-#
-#         try:
-#             latest_rate = Rate.objects.filter(
-#                 base_currency_type=base_currency_type,
-#                 currency_type=currency_type,
-#                 source=source,
-#             ).latest('created')
-#         except Rate.DoesNotExist:
-#             latest_rate = None
-#
-#         if latest_rate is None or \
-#                 latest_rate.sale != sale or \
-#                 latest_rate.buy != buy:
-#             Rate.objects.create(
-#                 base_currency_type=base_currency_type,
-#                 currency_type=currency_type,
-#                 buy=buy,
-#                 sale=sale,
-#                 source=source,
-#             )
-#
-#
-# def start_parse():
-#     for _ in range(5):
-#         parse_url(17, 10, 2022)
+from json import JSONDecodeError
+
+from currency.utils import to_decimal
+from currency.models import Rate, Source
+from currency import consts
+
+import requests
+from datetime import datetime
+
+
+DATE = f"{datetime.now():%d.%m.%Y}".split('.')
+
+
+def parse_url(day=DATE[0], month=DATE[1], year=DATE[2]):
+    count = 0
+    date_parse = []
+
+    while count != 3:
+
+        day = day if len(date_parse) == 0 else int(date_parse[0]) - 1
+        month = month if len(date_parse) == 0 else int(date_parse[1])
+        year = year if len(date_parse) == 0 else int(date_parse[2])
+
+        url = f"https://api.privatbank.ua/p24api/exchange_rates?json&date={day}.{month}.{year}"
+
+        response = requests.get(url)
+        response_data = response.json()
+        if len(response_data['exchangeRate']) == 0:
+            count += 1
+            continue
+        else:
+            count = 0
+            write_to_db(response_data, url)
+
+        date = response.json()['date']
+        date_parse.clear()
+
+        for x in date.split('.'):
+            date_parse.append(x)
+
+
+def write_to_db(response_data, url):
+
+    source = Source.objects.get_or_create(
+        name=consts.CODE_NAME_PRIVATBANK,
+        defaults={'source_url': url, 'name': 'PrivatBank'},)[0]
+
+    for rate_data in response_data['exchangeRate'][1:]:
+        try:
+            currency_type = rate_data['currency']
+            base_currency_type = rate_data['baseCurrency']
+            buy = to_decimal(rate_data['purchaseRate'])
+            sale = to_decimal(rate_data['saleRate'])
+            created = response_data['date']
+        except KeyError:
+            continue
+        except JSONDecodeError:
+            continue
+
+        try:
+            latest_rate = Rate.objects.filter(
+                base_currency_type=base_currency_type,
+                currency_type=currency_type,
+                source=source,
+                created=created,
+            ).latest('created')
+        except Rate.DoesNotExist:
+            latest_rate = None
+
+        if latest_rate is None or \
+                latest_rate.sale != sale or \
+                latest_rate.buy != buy:
+            Rate.objects.create(
+                base_currency_type=base_currency_type,
+                currency_type=currency_type,
+                buy=buy,
+                sale=sale,
+                source=source,
+                created=created,
+            )
